60b4442df1142c6ad5c77357d47a233411bc7c8e
22t1080025 <22t1080025@husc.edu.vn>
2026-07-26 19:44:31 +0700

    Add Post and commit

--- obj/Debug/net10.0/BlogManager_PhanThePho.AssemblyInfo.cs
+++ obj/Debug/net10.0/BlogManager_PhanThePho.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogManager_PhanThePho")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1391b7409bfc7ca95a35ade6346e4f41e70a32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5a2820afd6922e7b90e8ba92e7989b7cb2967d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogManager_PhanThePho")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogManager_PhanThePho")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
